ea311a9acc711e897ea784ba24c1c67ff641e1af
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-08-05 02:27:43 +0530

    More Stuff

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -28,12 +28,13 @@ app.get('/health', (req, res) => res.status(200).send('OK'));
 
 // Production setup
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
     });
 }
+
 
 app.use(errorHandler);
 
